6cfbd8cf7d82986a2b3011900a99cef195055912
Young9598 <yymmchang@gmail.com>
2025-07-21 16:02:59 +0800

    support binary mode for prefix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# main.py (整合後)
+# main.py（已整合 binary mode 支援）
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import os
@@ -7,6 +7,10 @@ import hashlib
 
 from GBF import gbf_gen, BF, hashs, size
 from PPDI import gbf_merge, compare, verify
+
+# === Global Parameters ===
+MAX_PREFIX_LEN = 32      # prefix 拆分最大長度 ℓ
+BINARY_PREFIX_MODE = True  # 是否使用 binary 編碼
 
 app = Flask(__name__)
 ALLOWED_EXTENSIONS = {'txt'}
@@ -30,20 +34,25 @@ def import_data(filename):
     with open(filename, 'r') as f:
         return f.readlines()
 
-def get_prefixes(label):
-    return [label[:i] for i in range(1, len(label)+1)]
+def get_prefixes(label, max_prefix_len=None, binary_mode=False):
+    if binary_mode:
+        bin_label = ''.join(format(ord(c), '08b') for c in label)
+        L = len(bin_label)
+        ℓ = min(L, max_prefix_len) if max_prefix_len else L
+        return [bin_label[:i] for i in range(1, ℓ + 1)]
+    else:
+        L = len(label)
+        ℓ = min(L, max_prefix_len) if max_prefix_len else L
+        return [label[:i] for i in range(1, ℓ + 1)]
 
 def flip_last_bit(s):
-    if s[-1] == '0':
-        return s[:-1] + '1'
-    else:
-        return s[:-1] + '0'
+    return s[:-1] + ('1' if s[-1] == '0' else '0')
 
-def preprocess(filename, cname):
+def preprocess(filename, cname, max_prefix_len=MAX_PREFIX_LEN, binary_mode=BINARY_PREFIX_MODE):
     tmp, tmp1 = [], []
     for line in files[filename]:
         x, label = line.strip().split(',')
-        prefixes = get_prefixes(label)
+        prefixes = get_prefixes(label, max_prefix_len, binary_mode)
         if filename == cname:
             for p in prefixes:
                 tmp.append(f"{x}||{p}")
@@ -104,7 +113,7 @@ def validate():
 
     for fname in [client_name, sender_name]:
         files[fname] = import_data(fname + '.txt')
-        preprocess(fname, client_name)
+        preprocess(fname, client_name, max_prefix_len=MAX_PREFIX_LEN, binary_mode=BINARY_PREFIX_MODE)
 
     comparec = PPDImain(client_name, sender_name)
     intersection = checkintersection(comparec, client_name)
